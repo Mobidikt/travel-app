a788ad9c46b7165baf65f0d5b2e9d3ae5f1be518
IKLOA <fandor1q@yandex.com>
2021-03-05 19:38:23 +0300

    feat: add footer and remove unnecessary layer

--- frontend/src/layouts/DefaultLayout.tsx
+++ frontend/src/layouts/DefaultLayout.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
 import { Layout } from 'antd'
+import { Outlet } from 'react-router-dom'
+import logo from '../assets/rs-logo.png'
+import './DefaultLayout.scss'
+
+const { Header, Content, Footer } = Layout
 
 const DefaultLayout: React.FC = () => (
   <Layout className="default-layout">
-    <h1>Default</h1>
+    {/* <Header /> */}
+    <Content className="app-content">
+      <Outlet />
+    </Content>
+    <Footer className="app-footer">
+      <ul className="app-footer-list">
+        <li className="app-footer-list__item">
+          <a href="https://github.com/Mobidikt">Krill Metsker</a>
+        </li>
+        <li className="app-footer-list__item">
+          <a href="https://github.com/ArtyomMakarov">Artsem Makarov</a>
+        </li>
+        <li className="app-footer-list__item">
+          <div className="logo-container">
+            <a href="https://rs.school/">
+              <img className="rs-logo" src={logo} alt="rs-school-logo" />
+            </a>
+            <p>2021</p>
+          </div>
+        </li>
+        <li className="app-footer-list__item">
+          <a href="https://github.com/anzhelaAbitova">Anzhela Abitova</a>
+        </li>
+        <li className="app-footer-list__item">
+          <a href="https://github.com/IKLOA">Andrey Gavrilov</a>
+        </li>
+      </ul>
+    </Footer>
   </Layout>
 )
 
--- frontend/src/router/routes.tsx
+++ frontend/src/router/routes.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import { PartialRouteObject } from 'react-router'
 
-import AuthLayout from '../layouts/AuthLayout'
 import DefaultLayout from '../layouts/DefaultLayout'
 
 const routes: PartialRouteObject[] = [
   {
     path: '/',
     element: <DefaultLayout />,
-  },
-  {
-    path: 'auth',
-    element: <AuthLayout />,
   },
 ]
 
