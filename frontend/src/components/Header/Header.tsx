62c304a7c554bd99faa06fd0c3520851eec1672b
Kirill Mecker <ghost3299@yandex.ru>
2021-03-07 10:37:47 +0400

    feat: header menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,15 +1,33 @@
+import { Button } from 'antd'
 import React from 'react'
+import { useLocation } from 'react-router'
 import logo from '../../assets/logo.png'
+import LanguageSelect from '../LanguageSelect/LanguageSelect'
 import Search from '../Search/Search'
 import './Header.scss'
 
+const isLogin = false
 // eslint-disable-next-line
 function Header() {
+  const { pathname } = useLocation()
+  const mainLocation = pathname === '/countries'
   return (
     <div className="header">
       <div className="header__wrapper">
-        <img src={logo} className="header__logo" alt="logo" />
-        <Search />
+        <a href="/countries">
+          <img src={logo} className="header__logo" alt="logo" />
+        </a>
+        <div className="header__menu">
+          <LanguageSelect />
+          {mainLocation ? <Search /> : null}
+          {isLogin ? (
+            <button type="button">Выйти</button>
+          ) : (
+            <Button className="header__btn" shape="round" size="large">
+              Авторизоваться
+            </Button>
+          )}
+        </div>
       </div>
       <h1 className="header__title">Travel app</h1>
     </div>
