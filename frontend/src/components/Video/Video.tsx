4426c68a2649e4ffc466922bd9e6468582f25789
Kirill Mecker <ghost3299@yandex.ru>
2021-03-07 17:13:36 +0400

    feat: styles

--- frontend/src/components/Video/Video.tsx
+++ frontend/src/components/Video/Video.tsx
@@ -7,14 +7,15 @@ function Video() {
     <section className="video">
       <div className="video__wrapper">
         <h3 className="video__title">Video about country</h3>
-        <iframe
-          className="video__iframe"
-          title="video-country"
-          src="https://www.youtube.com/embed/tQIgwox2nmA"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
+        <div className="video__iframe">
+          <iframe
+            title="video-country"
+            src="https://www.youtube.com/embed/tQIgwox2nmA"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        </div>
       </div>
     </section>
   )
