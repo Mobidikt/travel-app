2ee39e941d6b713515cdd5dfc02333fe3e306916
ArtyomMakarov <Makar6031994@gmail.com>
2021-03-08 16:32:48 +0300

    feat: create country card

--- frontend/src/pages/Countries/Countries.tsx
+++ frontend/src/pages/Countries/Countries.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import AuthCard from '../../components/AuthCard/AuthCard'
 import './Countries.scss'
+import CountryCard from '../../components/CountryCard/CountryCard'
 
 const Countries: React.FC = () => {
   return (
     <div className="countries-page">
       <h1>Countries</h1>
       <AuthCard />
+      <CountryCard />
     </div>
   )
 }
