ff90df7f9969a105bc0da117b44e9127d0d09879
Kirill Mecker <ghost3299@yandex.ru>
2021-03-10 20:58:37 +0400

    fix: styles

--- frontend/src/store/reducers/countries.ts
+++ frontend/src/store/reducers/countries.ts
@@ -55,6 +55,56 @@ const initialState: CountriesState = {
         'https://im0-tub-ru.yandex.net/i?id=c88f410e342d984a30f1622b3e509a80-l&n=33&w=540&h=720&q=60',
       video: 'https://www.youtube.com/embed/gwgIiYI43qs',
     },
+    {
+      id: 24576568,
+      idCountry: 5,
+      country: 'Германия',
+      population: 83149300,
+      capital: 'Берлин',
+      currency: 'евро',
+      description:
+        'Государство в Центральной Европе. Площадь территории - 357 408,74 км². Численность населения на 30 сентября 2019 года - 83 149 300 жителей. Занимает 18-е место в мире по численности населения и 62-е в мире по территории.',
+      picture: 'https://www.1zoom.ru/big2/982/324344-svetik.jpg',
+      video: 'https://www.youtube.com/embed/hbb4PYiSnVs',
+    },
+    {
+      id: 35876568,
+      idCountry: 6,
+      country: 'Ирландия',
+      population: 4977400,
+      capital: 'Дублин',
+      currency: 'евро',
+      description:
+        'Государство в Западной Европе, занимающее большую часть острова Ирландия. На севере граничит с Северной Ирландией. Площадь - 70,2 тыс. км². Название страны происходит от Eire «государство».',
+      picture:
+        'https://im0-tub-ru.yandex.net/i?id=39f0118c1171dfc754ad6a425c68e615-l&n=33&w=480&h=720&q=60',
+      video: 'https://www.youtube.com/embed/GSlEfFM7CFo',
+    },
+    {
+      id: 42076568,
+      idCountry: 7,
+      country: 'Грузия',
+      population: 3716858,
+      capital: 'Тбилиси',
+      currency: 'грузинский лари',
+      description:
+        'Государство, расположенное в западной части Закавказья на восточном побережье Чёрного моря. Относится к Передней Азии; рассматривается часто как страна на стыке Европы и Азии, иногда - как часть современной Европы.',
+      picture:
+        'https://cdn.fishki.net/upload/post/2019/05/22/2985752/015ae4ee2b27057390111dab27044553.jpg',
+      video: 'https://www.youtube.com/embed/0xluX6E2tKo',
+    },
+    {
+      id: 45076568,
+      idCountry: 8,
+      country: 'Эстония',
+      population: 1329460,
+      capital: 'Таллин',
+      currency: 'евро',
+      description:
+        'Государство, расположенное в Северной Европе на восточном побережье Балтийского моря, омываемое водами Финского и Рижского заливов. На востоке граничит с Россией, на юге - с Латвией. На севере, в Финском заливе, проходит морская граница с Финляндией. На севере и западе омывается водами Балтийского моря.',
+      picture: 'https://im0-tub-ru.yandex.net/i?id=74dcfc8ea1de9c8f9c102b1f5276be26&n=13',
+      video: 'https://www.youtube.com/embed/fibYq834w14',
+    },
   ],
   isLoading: false,
   isError: false,
