153dd59a6acd40e5e04fa258f25bd00d6f3c4f98
anzhelaAbitova <55100446+anzhelaAbitova@users.noreply.github.com>
2021-03-11 21:21:52 +0400

    fix: env db connection

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 mongoose.set('useCreateIndex', true)
 
-const uri = 'mongodb://localhost/test'
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function () {
